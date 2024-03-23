9ebd6febcec6863ebd5088eca612a139655a96ba
SOUnit <workbookwork7@gmail.com>
2024-03-23 14:14:08 -0700

    add login route

--- 01-cookie-node-js/app.js
+++ 01-cookie-node-js/app.js
@@ -5,6 +5,27 @@ const app = express();
 app.set("view engine", "ejs");
 
 // route
+app.get("/login", function (req, res) {
+  res.render("pages/login");
+});
+
+app.post("/login", function (req, res) {
+  const { email, password } = req.body;
+  console.log(email, password);
+
+  // Actual implementation would check values in a database
+  if (email === "t@t.com" && password === "password") {
+    req.session.loggedIn = true;
+  }
+
+  res.redirect("/");
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {});
+  res.redirect("/");
+});
+
 app.get("/", function (req, res) {
   res.render("pages/index");
 });
